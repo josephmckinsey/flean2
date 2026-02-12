854544825bbb3d7f616f228a5900885ab71f6a5a
Joseph Mckinsey <joseph.mckinsey@digitalgeospecialists.com>
2026-02-12 14:55:06 -0700

    Attempt 2 of normal numbers (better)

--- Flean2/Basic.lean
+++ Flean2/Basic.lean
@@ -8,6 +8,7 @@ import Mathlib.Analysis.Normed.Module.Basic
 import Mathlib.Algebra.Order.Floor.Div
 import Mathlib.Data.Int.Log
 import Flean2.RoundNearest
+import Mathlib.Data.Int.SuccPred
 
 section
 
@@ -540,23 +541,93 @@ theorem round_near_all_at_places (prec : ℕ) (e : ℤ) (x : X)
 -- between r '' [2^prec, 2^(prec+1)] and r1 '' [2^prec, 2^(prec + 1)]
 -- φ ∘ r = r1
 --
+end
+
+section
 
 -- This one is a partial order.
-structure NormalNumber (p : ℕ) extends DyadicPair where
-  bound : 2^p <= m ∧ m < 2^(p + 1) := by norm_num
+@[ext]
+structure NormalNumber (p : ℕ) where
+  m : ℤ
+  e : ℤ
+  bound : 2^p <= m ∧ m < 2^(p + 1) := by simp
 
-attribute [coe] NormalNumber.toDyadicPair
-instance {p : ℕ} : CoeHead (NormalNumber p) DyadicPair := ⟨NormalNumber.toDyadicPair⟩
+def NormalNumber.interp {p : ℕ} (X : Type*) [Field X] (f : NormalNumber p) : X :=
+  f.m * 2^f.e
+
+def NormalNumber.interp_pos {p : ℕ} (X : Type*)
+    [Field X] [LinearOrder X] [IsStrictOrderedRing X] (f : NormalNumber p) :
+    0 < f.interp X := by
+  unfold interp
+  have := f.bound.1
+  refine mul_pos (Int.cast_pos.mpr ?_) ?_
+  · apply lt_of_lt_of_le ?_ f.bound.1
+    positivity
+  positivity
+
+theorem NormalNumber.interp_bound {p : ℕ} (X : Type*) [Field X] [LinearOrder X]
+    [IsStrictOrderedRing X] [FloorRing X] (f : NormalNumber p) :
+    2^(f.e + p) ≤ f.interp X ∧ f.interp X < 2^(f.e + p + 1):= by
+  unfold interp
+  constructor
+  · rw [add_comm, zpow_add₀ (by norm_num)]
+    apply (mul_le_mul_iff_left₀ (by positivity)).mpr
+    exact_mod_cast f.bound.1
+  rw [add_assoc, add_comm, zpow_add₀ (by norm_num)]
+  apply (mul_lt_mul_iff_left₀ (by positivity)).mpr
+  exact_mod_cast f.bound.2
+
+theorem Int.log_eq_iff {R : Type*} [Semifield R] [LinearOrder R]
+    [IsStrictOrderedRing R] [FloorSemiring R] {b : ℕ} (hb : 1 < b) {x : ℤ}
+    {r : R} (hr : 0 < r) : Int.log b r = x ↔ b^x <= r ∧ r < b^(x+1) :=
+  Iff.intro
+    (fun h ↦ h.symm ▸ ⟨zpow_log_le_self hb hr, lt_zpow_succ_log_self hb r⟩)
+    (fun ⟨h1, h2⟩ ↦ le_antisymm
+      (Order.le_of_lt_add_one <| (lt_zpow_iff_log_lt hb hr).mp h2)
+      ((zpow_le_iff_le_log hb hr).mp h1))
+
+theorem NormalNumber.log2_interp {p : ℕ} (X : Type*) [Field X] [LinearOrder X]
+    [IsStrictOrderedRing X] [FloorRing X] (f : NormalNumber p) :
+    Int.log 2 (f.interp X) = f.e + p := by
+  apply (Int.log_eq_iff (by norm_num) (by exact_mod_cast f.interp_pos X)).mpr
+  exact_mod_cast f.interp_bound X
+
+theorem NormalNumber.interp_injective {p : ℕ} (X : Type*)
+    [Field X] [LinearOrder X] [IsStrictOrderedRing X] [FloorRing X] :
+    Function.Injective (NormalNumber.interp X (p := p)) := by
+  intro f1 f2 h
+  have e_eq: f1.e = f2.e := by
+    rw [<-add_left_inj, <-log2_interp X f1, <-log2_interp X f2, h]
+  rw [interp, interp, e_eq,
+    mul_right_cancel_iff_of_pos (by positivity), Int.cast_inj] at h
+  exact NormalNumber.ext h e_eq
 
 instance : PartialOrder (NormalNumber p) where
-  le := sorry
-  le_refl := sorry
-  le_trans := sorry
-  le_antisymm := sorry
+  le x y := x.interp ℚ <= y.interp ℚ
+  le_refl := by simp
+  le_trans _ _ _ ha hb := le_trans ha hb
+  le_antisymm a b ha hb := NormalNumber.interp_injective ℚ
+    (le_antisymm ha hb)
 
-#check ((.mk ⟨4, 0⟩ : NormalNumber 2) : ℝ)
+theorem NormalNumber.interp_X_rat (X : Type*)
+    [Field X] [LinearOrder X] [IsStrictOrderedRing X]
+    (f : NormalNumber p) : (interp ℚ f : X) = interp X f := by
+  simp [interp]
 
-#check NormalNumber.toDyadicPair
+theorem NormalNumber.le_def (X : Type*)
+    [Field X] [LinearOrder X] [IsStrictOrderedRing X]
+    (f1 f2 : NormalNumber p) : f1 <= f2 ↔ f1.interp X <= f2.interp X := by
+  change f1.interp ℚ <= f2.interp ℚ ↔ f1.interp X <= f2.interp X
+  rw [<-f1.interp_X_rat X, <- f2.interp_X_rat X, Rat.cast_le]
+
+theorem NormalNumber.interp_strictMono (X : Type*)
+    [Field X] [LinearOrder X] [IsStrictOrderedRing X] [FloorRing X] :
+    StrictMono (NormalNumber.interp X (p := p)) :=
+  Monotone.strictMono_of_injective
+    (fun x y h ↦ (le_def X x y).mp h)
+    (interp_injective X)
+
+#check (.mk 4 0 : NormalNumber 2)
 
 def round_near_all' (prec : ℕ) (x : X) : NormalNumber prec :=
   ⟨round_near_all prec x, round_near_all_mantissa prec x⟩
