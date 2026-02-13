c4c4b96419332d7669d16a0f4ebc8c5e91ffbbc1
Joseph Mckinsey <joseph.mckinsey@digitalgeospecialists.com>
2026-02-13 15:25:05 -0700

    Normal numbers round 1

--- Flean2/Basic.lean
+++ Flean2/Basic.lean
@@ -9,6 +9,7 @@ import Mathlib.Algebra.Order.Floor.Div
 import Mathlib.Data.Int.Log
 import Flean2.RoundNearest
 import Mathlib.Data.Int.SuccPred
+import Mathlib.Order.Interval.Set.SurjOn
 
 section
 
@@ -351,6 +352,48 @@ def PartialRounder.iUnion {ι : Type*} [Preorder ι] {s : ι → Set X} {t : ι 
 
 end
 
+section
+
+variable {X : Type*} {F : Type*} [LinearOrder X] [PartialOrder F]
+variable {i : F → X} {r : X → F}
+
+theorem ValidRounder.mapsTo_Icc {a b : F} (approx : ValidRounder i r) :
+    Set.MapsTo r (Set.Icc (i a) (i b)) (Set.Icc a b) := by
+  nth_rw 2 [<-approx.left_inverse a, <-approx.left_inverse b]
+  exact approx.r_mono.mapsTo_Icc
+
+theorem ValidRounder.Icc {a b : F} (approx : ValidRounder i r) (h : a ≤ b) :
+    r '' (Set.Icc (i a) (i b)) = Set.Icc a b := by
+  rw [@Set.image_eq_iff_surjOn_mapsTo]
+  constructor
+  · nth_rw 2 [<-approx.left_inverse a, <-approx.left_inverse b]
+    exact surjOn_Icc_of_monotone_surjective approx.r_mono approx.left_inverse.surjective
+      (approx.i_mono h)
+  exact approx.mapsTo_Icc
+
+theorem PartialRounder.r_le_f_of_x_le_f (approx : PartialRounder i r s) {x : X} {f : F}
+    (hx : x ∈ s) (hf : f ∈ r '' s) (h : x ≤ i f) : r x ≤ f :=
+  approx.left_inverse hf ▸ approx.r_mono hx (approx.i_map hf) h
+
+theorem PartialRounder.mapsTo_Icc {a b : F} (approx : PartialRounder i r s)
+    (ha : a ∈ r '' s) (hb : b ∈ r '' s) (hs : Set.Icc (i a) (i b) ⊆ s) :
+    Set.MapsTo r (Set.Icc (i a) (i b)) (Set.Icc a b) := by
+  nth_rw 2 [<-approx.left_inverse ha, <-approx.left_inverse hb]
+  exact (approx.r_mono.mono hs).mapsTo_Icc
+
+theorem PartialRounder.Icc_ofSurjOn {a b : F} (approx : PartialRounder i r s)
+    (h : Set.SurjOn r s .univ) (hs : Set.Icc (i a) (i b) ⊆ s)
+    : r '' (Set.Icc (i a) (i b)) = (Set.Icc a b) := by
+  let target : ∀{a}, a ∈ r '' s := fun {f} ↦ h (Set.mem_univ f)
+  rw [@Set.image_eq_iff_surjOn_mapsTo]
+  exact And.intro
+    (fun f fh ↦
+      ⟨i f, ⟨approx.i_mono target target fh.1, approx.i_mono target target fh.2⟩,
+        approx.left_inverse target⟩)
+    (approx.mapsTo_Icc target target hs)
+
+end
+
 /-!
 # Why no Add instance?
 
@@ -446,17 +489,21 @@ def validRounder_round_near : ValidRounder ((↑) : ℤ → X) round_near where
   i_mono := Int.cast_mono
   left_inverse := round_near_leftInverse
 
-def validRounder_fixedPoint (e : ℤ) := validRounder_round_near.mul (
-  show 0 < (2 : X)^e by positivity
-)
 
 /-
 TODO: m 2^e with 2^prec <= m < 2^(prec+1)
 TODO: Make a round_fixed_point
 -/
 
+def round_near_e (e : ℤ) (x : X) : ℤ := round_near (x / 2^e)
+def interp_e (e : ℤ) (f : ℤ) : X := ↑f * 2^e
+
+def validRounder_round_near_e (e : ℤ) :
+    ValidRounder (interp_e e (X := X)) (round_near_e e) :=
+  validRounder_round_near.mul (by positivity)
+
 def mantissaPartialRounder_round_near (e : ℤ) (prec : ℕ) :
-    PartialRounder (fun f ↦ ↑f * 2^e : ℤ → X) (fun x ↦ round_near (x / 2^e) : X → ℤ)
+    PartialRounder (interp_e e) (round_near_e e)
     (Set.Icc ((2 : X)^(prec + e)) (2^(prec + e + 1))) := by
   rw [show (2 : X)^(prec + e) = (2^prec : ℤ) * (2 : X)^e by
     rw [zpow_add₀ (by norm_num)]
@@ -465,51 +512,137 @@ def mantissaPartialRounder_round_near (e : ℤ) (prec : ℕ) :
     rw [add_assoc, add_comm e 1, <-add_assoc, zpow_add₀ (by norm_num)]
     norm_cast
   ]
-  have approx := validRounder_fixedPoint e (X := X)
+  have approx := validRounder_round_near_e e (X := X)
   exact ValidRounder.toPartialRounderOfMapTo approx fun x xh ↦
     ⟨approx.i_mono (approx.f_le_r_of_f_le_x xh.1),
     approx.i_mono (approx.r_le_f_of_x_le_f xh.2)⟩
 
-def round_near_e_and_succ (e : ℤ) (prec : ℕ) := fun (x : X) ↦ if x < 2^(prec + e + 1) then
-    (round_near (x / 2^e), e) -- TODO: add normalization here?
-  else
-    (round_near (x / 2^(e+1)), e + 1)
+-- TODO: Generalize
+theorem round_near_image (a b : ℤ) (h : a ≤ b) :
+    round_near '' (Set.Icc (a : X) (b : X)) = Set.Icc a b :=
+  (validRounder_round_near (X := X)).Icc h
 
-structure DyadicPair where
-  m : ℤ
-  e : ℤ
+theorem round_near_e_image (prec : ℕ) :
+    (round_near_e e) '' (Set.Icc ((2 : X)^(prec + e)) (2^(prec + e + 1))) =
+    Set.Icc (2^prec) (2^(prec + 1)) := by
+  suffices round_near_e (X := X) e ''
+      Set.Icc (interp_e e (2 ^ prec)) (interp_e e (2 ^ (prec + 1))) =
+      Set.Icc (2 ^ prec) (2 ^ (prec + 1)) by
+    rw [interp_e, interp_e] at this
+    convert this
+    · simp [Int.cast_pow, zpow_add₀]
+    rw [zpow_add_one₀ (by norm_num), zpow_add₀ (by norm_num), pow_succ]
+    field_simp
+    norm_cast
+  exact (validRounder_round_near_e e).Icc (pow_le_pow_right₀ (by norm_num) (by linarith))
 
-instance [Field k] : Coe DyadicPair k where
-  coe x := x.m * (2 : k)^x.e
+abbrev interp_pair (X : Type*) [Field X] (f : ℤ × ℤ) : X := f.1 * (2 : X)^f.2
 
-def normalize_edge (prec : ℕ) : DyadicPair → DyadicPair := fun ⟨m, e⟩ ↦
+def normalize_edge (prec : ℕ) : ℤ × ℤ → ℤ × ℤ := fun ⟨m, e⟩ ↦
   if m = 2^(prec+1) then ⟨2^prec, e + 1⟩ else ⟨m, e⟩
 
-theorem normalize_edge_cast_comm [Field k] [NeZero (2 : k)] (prec : ℕ) (f : DyadicPair) :
-    ((normalize_edge prec f) : k) = f := by
-  unfold normalize_edge
-  if h : f.m = 2^(prec + 1) then
+theorem normalize_edge_cast_comm [Field k] [NeZero (2 : k)] (prec : ℕ)
+    (f : ℤ × ℤ) : (interp_pair k (normalize_edge prec f)) = interp_pair k f := by
+  unfold normalize_edge interp_pair
+  if h : f.1 = 2^(prec + 1) then
     simp only [h, reduceIte, Int.cast_pow, Int.cast_ofNat]
     rw [pow_add, zpow_add₀ two_ne_zero]
     field_simp
   else
     simp_rw [if_neg h]
 
-theorem normalize_edge_ne2prec [Field k] [NeZero (2 : k)] (prec : ℕ) (f : DyadicPair) :
-    (normalize_edge prec f).m ≠ 2^(prec+1) := by
+theorem normalize_edge_narrows_prec_upper [Field k] [CharZero k] (prec : ℕ)
+    {f : ℤ × ℤ} (h : f.1 ≤ 2 ^ (prec + 1)) : (normalize_edge prec f).1 < 2^(prec+1) := by
   unfold normalize_edge
   grind [pow_right_inj₀]
 
+theorem normalize_edge_narrows_prec_lower [Field k] [CharZero k] (prec : ℕ)
+    {f : ℤ × ℤ} (h : 2 ^ prec ≤ f.1) : 2^prec ≤ (normalize_edge prec f).1 := by
+  unfold normalize_edge
+  grind only
 
 def round_near_all (prec : ℕ) := fun (x : X) ↦
   let e := Int.log 2 x - prec
   -- 2^(e + prec) <= x < 2^(e + prec + 1) for x ≠ 0
   -- So 2^prec <= round_near (x / 2^e) <= 2^(prec + 1)
   -- Normalization trims off the edge to [2^prec, 2^(prec + 1))
-  normalize_edge prec ⟨round_near (x / 2^e), e⟩
+  normalize_edge prec ⟨round_near_e e x, e⟩
 
-theorem round_near_all_mantissa (prec : ℕ) (x : X) :
-    2^prec <= (round_near_all prec x).m ∧ (round_near_all prec x).m < 2^(prec + 1) := by sorry
+theorem round_near_e_mantissa (prec : ℕ) {x : X} (h : 0 < x) :
+    2^prec ≤ (x / 2^(Int.log 2 x - prec)) ∧
+    (x / 2^(Int.log 2 x - prec)) < 2^(prec+1) := by
+  set y := x / 2^(Int.log 2 x - prec) with y_def
+  rw [sub_eq_add_neg, div_eq_mul_inv, <-zpow_neg, neg_add_rev, neg_neg,
+    zpow_add₀ (by norm_num), mul_comm, mul_assoc, mul_comm _ x, zpow_neg] at y_def
+  have rhs : x * (2^(Int.log 2 x))⁻¹ < 2 := by
+    rw [mul_inv_lt_iff₀ (by positivity), mul_comm,
+      <-zpow_add_one₀ (by norm_num)]
+    exact Int.lt_zpow_succ_log_self (by norm_num) x
+  have lhs : 1 ≤ x * (2^(Int.log 2 x))⁻¹ := by
+    rw [le_mul_inv_iff₀ (by positivity), mul_comm, mul_one]
+    apply Int.zpow_log_le_self (by norm_num) h
+  suffices 1 ≤ x * (2^(Int.log 2 x))⁻¹ ∧ x * (2^(Int.log 2 x))⁻¹ < 2 by
+    simpa [pow_succ, y_def]
+  exact ⟨lhs, rhs⟩
+
+
+theorem round_near_all_mantissa [NeZero (2 : X)] (prec : ℕ) {x : X} (h : 0 < x) :
+    2^prec <= (round_near_all prec x).1 ∧ (round_near_all prec x).1 < 2^(prec + 1) := by
+  constructor
+  · apply normalize_edge_narrows_prec_lower (k := X)
+    apply validRounder_round_near.f_le_r_of_f_le_x
+    exact_mod_cast (round_near_e_mantissa prec h).1
+  apply normalize_edge_narrows_prec_upper (k := X)
+  apply validRounder_round_near.r_le_f_of_x_le_f
+  exact_mod_cast le_of_lt (round_near_e_mantissa prec h).2
+
+theorem Int.log_eq_iff {R : Type*} [Semifield R] [LinearOrder R]
+    [IsStrictOrderedRing R] [FloorSemiring R] {b : ℕ} (hb : 1 < b) {x : ℤ}
+    {r : R} (hr : 0 < r) : Int.log b r = x ↔ b^x <= r ∧ r < b^(x+1) :=
+  Iff.intro
+    (fun h ↦ h.symm ▸ ⟨zpow_log_le_self hb hr, lt_zpow_succ_log_self hb r⟩)
+    (fun ⟨h1, h2⟩ ↦ le_antisymm
+      (Order.le_of_lt_add_one <| (lt_zpow_iff_log_lt hb hr).mp h2)
+      ((zpow_le_iff_le_log hb hr).mp h1))
+
+theorem round_near_e_zpow_eq (e : ℤ) (p : ℕ) : round_near_e e ((2 : X) ^ (e + p)) = 2^p :=
+  let i_eq : (2 : X) ^ (e + p) = interp_e e (2^p) := by
+    unfold interp_e
+    rw [add_comm]
+    simp [zpow_add₀]
+  i_eq ▸ (validRounder_round_near_e e).left_inverse _
+
+
+theorem round_near_all_top_eq (p : ℕ) (e : ℤ)
+    : interp_pair X (round_near_all p ((2 : X) ^ (e + ↑p + 1))) =
+      interp_pair X (2^p, e + 1) := by
+  rw [round_near_all, normalize_edge_cast_comm]
+  apply congr_arg
+  set x := (2 : X)^(e + p + 1) with x_def
+  have : Int.log 2 x - p = e + 1 := by
+    rw [x_def, show (2 : X) = ((2 : ℕ) : X) by norm_num,
+      Int.log_zpow (b := 2) (by norm_num)]
+    ring
+  rw [this, x_def, add_assoc, add_comm (p : ℤ) 1, <-add_assoc]
+  rw_mod_cast [round_near_e_zpow_eq (e + 1) p]
+  norm_cast
+
+theorem round_near_all_at_places (prec : ℕ) (e : ℤ) (x : X)
+    (h : 2 ^ (e + prec) <= x ∧ x ≤ 2 ^ (e + prec + 1)) :
+    interp_pair X (round_near_all prec x) = interp_e e (round_near_e e x) := by
+  by_cases x_eq : x = 2^(e + prec + 1)
+  · rw [x_eq, add_assoc]
+    rw_mod_cast [round_near_e_zpow_eq (X := X) e (prec + 1 )]
+    rw [Nat.cast_add, Nat.cast_one, <-add_assoc, round_near_all_top_eq,
+      interp_pair,interp_e, pow_succ, zpow_add_one₀ (by norm_num)]
+    field_simp
+    norm_cast
+  have xpos : 0 < x := lt_of_lt_of_le (by positivity) h.1
+  have : Int.log 2 x = e + prec := by
+    rw [Int.log_eq_iff (by norm_num) xpos]
+    exact ⟨h.1, lt_of_le_of_ne h.2 x_eq⟩
+  rw [round_near_all, normalize_edge_cast_comm, this, add_sub_cancel_right]
+  rfl
 
 -- For each i f ∈ X_i, there is a f' ∈ r_i '' X_i s.t. i' f' = i f.
 -- We need to take each f and assign it the original f' in X_i for all X_i that its a part of.
@@ -530,10 +663,6 @@ theorem round_near_all_mantissa (prec : ℕ) (x : X) :
 
 
 -- This is a proof by cases on the boundary.
-theorem round_near_all_at_places (prec : ℕ) (e : ℤ) (x : X)
-    (h : 2 ^ (e + prec) <= x ∧ x ≤ 2 ^ (e + prec + 1)) :
-    (round_near_all prec x : X) = (round_near (x / 2^e) : X) := by sorry
-
 
 
 -- PartialRounder i (round_near (x / 2^e), e) should be equivalent to PartialRounder i round_near (...)
@@ -555,15 +684,9 @@ structure NormalNumber (p : ℕ) where
 def NormalNumber.interp {p : ℕ} (X : Type*) [Field X] (f : NormalNumber p) : X :=
   f.m * 2^f.e
 
-def NormalNumber.interp_pos {p : ℕ} (X : Type*)
-    [Field X] [LinearOrder X] [IsStrictOrderedRing X] (f : NormalNumber p) :
-    0 < f.interp X := by
-  unfold interp
-  have := f.bound.1
-  refine mul_pos (Int.cast_pos.mpr ?_) ?_
-  · apply lt_of_lt_of_le ?_ f.bound.1
-    positivity
-  positivity
+def NormalNumber.interp_pos {p : ℕ} (X : Type*) [Field X] [LinearOrder X]
+    [IsStrictOrderedRing X] (f : NormalNumber p) : 0 < f.interp X :=
+   mul_pos (Int.cast_pos.mpr (lt_of_lt_of_le (by positivity) f.bound.1)) (by positivity)
 
 theorem NormalNumber.interp_bound {p : ℕ} (X : Type*) [Field X] [LinearOrder X]
     [IsStrictOrderedRing X] [FloorRing X] (f : NormalNumber p) :
@@ -577,14 +700,15 @@ theorem NormalNumber.interp_bound {p : ℕ} (X : Type*) [Field X] [LinearOrder X
   apply (mul_lt_mul_iff_left₀ (by positivity)).mpr
   exact_mod_cast f.bound.2
 
-theorem Int.log_eq_iff {R : Type*} [Semifield R] [LinearOrder R]
-    [IsStrictOrderedRing R] [FloorSemiring R] {b : ℕ} (hb : 1 < b) {x : ℤ}
-    {r : R} (hr : 0 < r) : Int.log b r = x ↔ b^x <= r ∧ r < b^(x+1) :=
-  Iff.intro
-    (fun h ↦ h.symm ▸ ⟨zpow_log_le_self hb hr, lt_zpow_succ_log_self hb r⟩)
-    (fun ⟨h1, h2⟩ ↦ le_antisymm
-      (Order.le_of_lt_add_one <| (lt_zpow_iff_log_lt hb hr).mp h2)
-      ((zpow_le_iff_le_log hb hr).mp h1))
+def NormalNumber.proj (f : NormalNumber p) : ℤ × ℤ := (f.m, f.e)
+
+theorem NormalNumber.interp_proj [Field X] {f : NormalNumber p} :
+    f.interp X = interp_pair X f.proj := by
+  simp [interp_pair, interp, proj]
+
+theorem NormalNumber.interp_e_proj [Field X] {f : NormalNumber p} :
+    f.interp X = interp_e f.e f.m := by
+  simp [interp_e, interp]
 
 theorem NormalNumber.log2_interp {p : ℕ} (X : Type*) [Field X] [LinearOrder X]
     [IsStrictOrderedRing X] [FloorRing X] (f : NormalNumber p) :
@@ -626,12 +750,6 @@ theorem NormalNumber.interp_strictMono (X : Type*)
   Monotone.strictMono_of_injective
     (fun x y h ↦ (le_def X x y).mp h)
     (interp_injective X)
-
-#check (.mk 4 0 : NormalNumber 2)
-
-def round_near_all' (prec : ℕ) (x : X) : NormalNumber prec :=
-  ⟨round_near_all prec x, round_near_all_mantissa prec x⟩
-
 
 -- I would like to prove that round_near_all' is a valid rounder on
 -- union e, 2^(e + prec) <= x <= 2^(e + prec + 1) = (0, infty)
@@ -774,6 +892,37 @@ structure WeakGlueData (ι : Type) [Preorder ι] (X : Type) [Preorder X] (F : Ty
 
 end
 
+def round_near_normal [Field X] [LinearOrder X] [IsStrictOrderedRing X] [FloorRing X]
+    (p : ℕ) (x : X) : NormalNumber p :=
+  if h : 0 < x then
+    match f_eq : round_near_all p x with
+    | ⟨m, e⟩ => .mk m e (by
+      have := round_near_all_mantissa (X := X) p h
+      rwa [f_eq] at this
+    )
+  else
+    .mk (2^p) (-p : ℤ) (by simp [pow_succ]) -- I chose 1 as junk
+
+theorem round_near_normal_eq [Field X] [LinearOrder X] [IsStrictOrderedRing X]
+    [FloorRing X] (p : ℕ) {x : X} (h : 0 < x) :
+    (round_near_normal p x).interp X = interp_pair X (round_near_all p x) := by
+  -- I love you grind
+  grind [NormalNumber.interp, round_near_normal]
+
+theorem round_normal_interp [Field X] [LinearOrder X]
+    [IsStrictOrderedRing X] [FloorRing X] (p : ℕ) :
+    Function.LeftInverse (round_near_normal p) (NormalNumber.interp X) := by
+  intro f
+  apply NormalNumber.interp_injective (X := X)
+  rw [round_near_normal_eq _ (f.interp_pos X),
+    round_near_all_at_places p f.e _ (by grind [NormalNumber.interp_bound]),
+    f.interp_e_proj, (validRounder_round_near_e (X := X) f.e).left_inverse f.m]
+
+theorem round_normal_monoOn [Field X] [LinearOrder X]
+    [IsStrictOrderedRing X] [FloorRing X] (p : ℕ) :
+    MonotoneOn (round_near_normal p) (Set.Ioi (0 : X)) := by
+  --apply MonotoneOn.union_lowerBound
+  sorry
 -- If i (r (i f)) = i f, and r' (i' f') = f', then
 --
 -- i i' r' r i i' f
